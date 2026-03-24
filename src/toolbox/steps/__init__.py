2dca1c9fd6f361cd914c20078351131537b9ac64
Orlando Prugel-Bennett <orlandopbennett@gmail.com>
2026-03-24 13:02:35 +0000

    changes based on comments from @Obsidian500

--- src/toolbox/steps/__init__.py
+++ src/toolbox/steps/__init__.py
@@ -33,10 +33,6 @@ STEP_CLASSES = {}
 """Dictionary mapping step names to their implementing classes."""
 QC_CLASSES = {}
 """Dictionary mapping QC test names to their implementing classes."""
-STEP_DEPENDENCIES = {
-    "QC: Salinity": ["Load OG1"],
-}
-"""Dictionary of explicit dependencies between steps by name."""
 
 
 def discover_steps():
