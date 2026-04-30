d9a6a306a31b3d47b823b55c01c0e8161557601f
Orlando Prugel-Bennett <orlandopbennett@gmail.com>
2026-04-30 15:34:01 +0100

    fixed testing

--- tests/test_impossible_location_qc.py
+++ tests/test_impossible_location_qc.py
@@ -7,13 +7,15 @@ from toolbox.steps.custom.qc.impossible_location_qc import impossible_location_q
 from utils.test_utils import create_mock_dataset
 
 
-def test_missing_variables():
+def test_missing_variables(caplog):
     data = xr.Dataset({"TEMP": ("N_MEASUREMENTS", [10.0, 12.0])})
     qc_step = impossible_location_qc(data)
     
-    # We expect the step to raise a KeyError because LATITUDE and LONGITUDE are missing
-    with pytest.raises(KeyError):
-        qc_step.return_qc()
+    flags = qc_step.return_qc()
+    
+    assert "LATITUDE or LONGITUDE missing" in caplog.text
+    assert "LATITUDE_QC" not in flags
+    assert "LONGITUDE_QC" not in flags
 
 
 @pytest.mark.parametrize("lats,lons,lat_flags,lon_flags", [
